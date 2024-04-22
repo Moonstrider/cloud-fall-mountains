7be2ed224ec0700349c779af5e660bd2b69c16ee
Robert <37094943+Moonstrider@users.noreply.github.com>
2024-04-22 14:29:14 +0800

    1

---
+++ notebook/src/main/java/com/cloudfall/mountains/notebook/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.cloudfall.mountains.notebook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        return new RedisTemplate<>();
+    }
+}
---
+++ notebook/src/main/java/com/cloudfall/mountains/notebook/controller/CacheController.java
@@ -0,0 +1,26 @@
+package com.cloudfall.mountains.notebook.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CacheController {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public CacheController(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/get")
+    public String getCache() {
+        return redisTemplate.opsForValue().get("a");
+    }
+
+    @GetMapping("/set")
+    public String setCache() {
+        redisTemplate.opsForValue().set("a", "aaa");
+        return "ok";
+    }
+}
