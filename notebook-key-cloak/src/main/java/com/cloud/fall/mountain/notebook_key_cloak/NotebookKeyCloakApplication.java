a4104a65a27aa57c83b2e6c826ce96f17ca64029
Robb <37094943+Moonstrider@users.noreply.github.com>
2024-10-08 15:43:44 +0800

    nextjs course and key cloak

---
+++ notebook-key-cloak/src/main/java/com/cloud/fall/mountain/notebook_key_cloak/NotebookKeyCloakApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.fall.mountain.notebook_key_cloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookKeyCloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookKeyCloakApplication.class, args);
+	}
+
+}
---
+++ notebook-key-cloak/src/test/java/com/cloud/fall/mountain/notebook_key_cloak/NotebookKeyCloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.fall.mountain.notebook_key_cloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookKeyCloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
