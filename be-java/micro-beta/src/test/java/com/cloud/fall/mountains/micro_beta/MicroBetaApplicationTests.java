0db244c6f87832c18c3e2b00e00f5c8b61dfc5d2
Robb <37094943+Moonstrider@users.noreply.github.com>
2025-04-14 16:53:51 +0800

    add 2 micro demo services

---
+++ be-java/micro-beta/src/test/java/com/cloud/fall/mountains/micro_beta/MicroBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.fall.mountains.micro_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
